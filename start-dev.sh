8f114e06e9295cb1ad1c4755faa5f7e6fa50a2ad
Ryan McDougall <rp.mcdougall@gmail.com>
2019-05-09 22:49:24 -0500

    copy good config on new install

--- start-dev.sh
+++ start-dev.sh
@@ -1,14 +1,36 @@
+function ProgressBar {
+  local duration=${1}
+
+
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    clean_line() { printf "\r"; }
+
+  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
+      already_done; remaining; percentage
+      sleep 1
+      clean_line
+  done
+  clean_line
+}
+
 
 docker-compose up -d
 
-docker exec -it janus-dev_cassandra_1 sed -i 's/start_rpc: false/start_rpc: true/' /etc/cassandra/cassandra.yaml
-docker restart janus-dev_cassandra_1
+echo "Waiting for Cassandra/Elastic to start up..."
+ProgressBar 120
+echo " "
+echo "Enable Thrift on Cassandra..."
+docker exec -it janus-dev_cassandra_1  nodetool enablethrift
 
 if [ ! -d "janusgraph-0.2.2-hadoop2" ]; then
+  echo "Installing Janus Gremnlin Server"
   wget https://github.com/JanusGraph/janusgraph/releases/download/v0.2.2/janusgraph-0.2.2-hadoop2.zip
   unzip janusgraph-0.2.2-hadoop2.zip
   cd janusgraph-0.2.2-hadoop2
-  sed -i "s/scriptEvaluationTimeout.*/scriptEvaluationTimeout: 180000/" conf/gremlin-server/gremlin-server-configuration.yaml
+  rm -rf conf/gremlin-server/gremlin-server-configuration.yaml
+  cp ../gremlin-server-configuration.yaml conf/gremlin-server/
   ./bin/gremlin-server.sh conf/gremlin-server/gremlin-server-configuration.yaml
 else
     cd janusgraph-0.2.2-hadoop2
